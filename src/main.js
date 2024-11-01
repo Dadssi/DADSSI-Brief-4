5b93b3c12c92c152641b191b94792580e9f6fa24
dadssi <dadssi.mohamed.abdelhak@gmail.com>
2024-11-01 17:26:14 +0100

    l'ajout d'une petite animation d'apparition de la pop Up

--- src/main.js
+++ src/main.js
@@ -2,11 +2,14 @@
 document.getElementById('btn-00').addEventListener('click', function() {
     console.log("hello");
     const textContainer = document.getElementById('ajout-tache-Container');
-    textContainer.style.display = "flex"; 
+    textContainer.style.display = "flex";
+    textContainer.style.scale = "1";
+    textContainer.style.transform = "all 1000ms ease-in-out"; 
 });
 // l'apparition de la pop Up d'ajout d'une tache
 document.getElementById('cancelBtn').addEventListener('click', function() {
     const textContainer = document.getElementById('ajout-tache-Container');
     textContainer.style.display = "none";
 });
+
 
