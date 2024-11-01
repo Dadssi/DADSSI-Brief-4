d924980b85475319c0360f31d2ed42909ab6c936
dadssi <dadssi.mohamed.abdelhak@gmail.com>
2024-11-01 17:15:02 +0100

    l'ajout de la fonction de fermeture de la pop up

--- src/main.js
+++ src/main.js
@@ -1,6 +1,12 @@
+// l'apparition de la pop Up d'ajout d'une tache
 document.getElementById('btn-00').addEventListener('click', function() {
     console.log("hello");
     const textContainer = document.getElementById('ajout-tache-Container');
     textContainer.style.display = "flex"; 
+});
+// l'apparition de la pop Up d'ajout d'une tache
+document.getElementById('cancelBtn').addEventListener('click', function() {
+    const textContainer = document.getElementById('ajout-tache-Container');
+    textContainer.style.display = "none";
 });
 
