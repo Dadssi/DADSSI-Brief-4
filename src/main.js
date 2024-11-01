5bc86f64a8a1b4fa14c5cb327efbc65bccf69a73
dadssi <dadssi.mohamed.abdelhak@gmail.com>
2024-11-01 16:59:32 +0100

    l'ajout du style à la pop Up de l'ajout d'une nouvelle tache

--- src/main.js
+++ src/main.js
@@ -1,2 +1,5 @@
-
+document.getElementById('btn-00').addEventListener('click', function() {
+    const textContainer = document.getElementById('ajout-tache-Container');
+    textContainer.style.display = "flex"; 
+});
 
