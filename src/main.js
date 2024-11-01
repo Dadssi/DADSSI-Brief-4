50d141c0f519b1bbdfc86930e9246a88e694559f
dadssi <dadssi.mohamed.abdelhak@gmail.com>
2024-11-01 17:12:17 +0100

    l'ajout de la fonction d'apparition de la pop up à partir du clique sur le bouton avec Js

--- src/main.js
+++ src/main.js
@@ -1,4 +1,5 @@
 document.getElementById('btn-00').addEventListener('click', function() {
+    console.log("hello");
     const textContainer = document.getElementById('ajout-tache-Container');
     textContainer.style.display = "flex"; 
 });
