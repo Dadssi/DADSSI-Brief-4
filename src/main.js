b5068c8d40dd28c90af817840a39de124271f7aa
dadssi <dadssi.mohamed.abdelhak@gmail.com>
2024-11-02 16:32:13 +0100

    Modification des parametre de l'animation de la Pop Up

--- src/main.js
+++ src/main.js
@@ -1,15 +1,12 @@
-// l'apparition de la pop Up d'ajout d'une tache
 document.getElementById('btn-00').addEventListener('click', function() {
-    console.log("hello");
     const textContainer = document.getElementById('ajout-tache-Container');
-    textContainer.style.display = "flex";
+    textContainer.style.display = "flex"; 
     textContainer.style.scale = "1";
-    textContainer.style.transform = "all 1000ms ease-in-out"; 
+    textContainer.style.opacity = "1";
 });
 // l'apparition de la pop Up d'ajout d'une tache
 document.getElementById('cancelBtn').addEventListener('click', function() {
     const textContainer = document.getElementById('ajout-tache-Container');
     textContainer.style.display = "none";
 });
-
 
